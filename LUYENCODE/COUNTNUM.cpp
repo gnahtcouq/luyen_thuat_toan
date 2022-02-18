5ebb13c2cf1f741f76fc8415ded8b5734a344a70
Quốc Thắng <comehere.thang@gmail.com>
2022-02-18 11:31:47 +0700

    18/2/22
    18/2/22

---
+++ LUYENCODE/COUNTNUM.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string s;
+  getline(cin, s);
+
+
+  int dem;
+  if (s[0] == ' ')
+    dem = 0;
+  else
+    dem = 1;
+  int length = s.length();
+  for (int i = 0; i < length - 1; i++)
+    if (s[i] == ' ' && s[i + 1] != ' ')
+      dem++;
+
+  cout << dem;
+
+  return 0;
+}
