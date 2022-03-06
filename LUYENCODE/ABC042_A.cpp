0e7edd8797059f9cfd895d051292d174e23bf3ea
Quốc Thắng <comehere.thang@gmail.com>
2022-03-06 15:05:31 +0700

    Update ABC042_A.cpp

--- LUYENCODE/ABC042_A.cpp
+++ LUYENCODE/ABC042_A.cpp
@@ -9,12 +9,7 @@ int main() {
 
   sort(a, a + n);
 
-  int b[3], m = 3;
-  b[0] = 5;
-  b[1] = 5;
-  b[2] = 7;
-
-  if (a[0] == b[0] && a[1] == b[1] && a[2] == b[2])
+  if (a[0] == 5 && a[1] == 5 && a[2] == 7)
     cout << "YES";
   else
     cout << "NO";
